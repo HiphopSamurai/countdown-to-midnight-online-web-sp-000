28268696f5bab11d1558a6c069786f1614f2956a
Garrett Thompson <gtcruz.gt@gmail.com>
2019-07-10 17:53:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,17 @@ def countdown
   x = 10
   while x >= 0
     puts "#{x} SECONDS!"
-    sleep(1)
     x -= 1
   end
   puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  x = 10
+  while x >= 0
+    puts "#{x} SECONDS!"
+    sleep(1)
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
