6ece49457e1616eb9c964e913fec90d10b7b6985
Garrett Thompson <gtcruz.gt@gmail.com>
2019-07-10 18:03:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 #write your code here
 def countdown(seconds)
-  while seconds >= 0
+  while seconds > 0
     puts "#{seconds} SECONDS!"
     seconds -= 1
   end
@@ -8,7 +8,7 @@ def countdown(seconds)
 end
 
 def countdown_with_sleep(seconds)
-  while seconds >= 0
+  while seconds > 0
     puts "#{seconds} SECONDS!"
     seconds -= 1
     sleep(1)
